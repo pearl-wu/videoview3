17e55a642e0c7a0903a8507c0b9d12b98f4e759b
pearl <lu92500@yahoo.com.tw>
2016-04-07 15:07:54 +0800

    Update videoplayer.js

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -8,20 +8,15 @@ module.exports = {
         topV: 0,
         leftV: 0,
     },
-
-    SCALING_MODE: {
-        SCALE_TO_FIT: 1,
-        SCALE_TO_FIT_WITH_CROPPING: 2
-    },
-
+    
     play: function (path, options, successCallback, errorCallback) {
         options = this.merge(this.DEFAULT_OPTIONS, options);
         exec(successCallback, errorCallback, "VideoPlayer", "play", [path, options]);
     },
     
-    preview: function(path, options, successCallback, errorCallback){
+    preview: function(path, successCallback, errorCallback){
         options = this.merge(this.DEFAULT_OPTIONS, options); 
-        exec(successCallback, errorCallback, "VideoPlayer", "preview", [path, options]);
+        exec(successCallback, errorCallback, "VideoPlayer", "preview", [path]);
     },
 
     stop: function (successCallback, errorCallback) {

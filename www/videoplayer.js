136482696b4b434784db3f50281658fc572dfd7e
pearl <lu92500@yahoo.com.tw>
2016-05-24 11:54:32 +0800

    Update videoplayer.js

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -2,29 +2,8 @@ var exec = require("cordova/exec");
 
 module.exports = {
 
-    _OPTIONS: {
-        widthV: 1028,
-        heightV: 760,
-        topV: 0,
-        leftV: 0
-    },
-    
-    beginning: function (options, successCallback, errorCallback) {
-      options = this.merge(this._OPTIONS, options);  
-        exec(successCallback, errorCallback, "VideoPlayer", "beginning", [options]);
-    },
-    
     many_play: function (paths, number, successCallback, errorCallback) {
-       
         exec(successCallback, errorCallback, "VideoPlayer", "many_play", [paths, number]);
-    },
-
-    play: function (path, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "VideoPlayer", "play", [path]);
-    },
-
-    stop: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "VideoPlayer", "stop", []);
     },
 
     merge: function () {

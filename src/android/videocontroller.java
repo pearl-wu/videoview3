8b44536abd0524773d31cdfc94f03a1f3717cfdc
pearl <lu92500@yahoo.com.tw>
2016-05-25 16:24:55 +0800

    Update videocontroller.java

--- src/android/videocontroller.java
+++ src/android/videocontroller.java
@@ -2,13 +2,18 @@ package com.bais.cordova.video;
 
 
 import cn.com.ebais.kyytvali.R;
+
 import java.util.ArrayList;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -31,22 +36,31 @@ public class videocontroller extends Activity{
 	 private ArrayList<String> mediaurls;
 	 private VideoView videoww;
 	 private MediaController mMediaController;
+	 private ImageView loading;
 
 	@SuppressLint("InlinedApi") public void onCreate(Bundle icicle) {
 
 		super.onCreate(icicle);
 		
+		
+		if(isNetworkConnected(this) == false){
+			Toast.makeText(getBaseContext(), "连线中断，请检查网路。", Toast.LENGTH_SHORT).show();		
+			this.finish();
+			return;
+		}
+		
+		
 		int currentOrientation = getResources().getConfiguration().orientation;	
 		switch(currentOrientation) {
-		      case Configuration.ORIENTATION_PORTRAIT:
-		    	// 旋轉為直立顯示
-		    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		       break;
-		}
-
-	requestWindowFeature(Window.FEATURE_NO_TITLE);
-	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	      case Configuration.ORIENTATION_PORTRAIT:
+	    	// 旋轉為直立顯示
+	    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	       break;
+	     }		
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         extras = getIntent().getExtras();
@@ -57,19 +71,19 @@ public class videocontroller extends Activity{
 		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 
 		FrameLayout frelLayout = new FrameLayout(this);
-		
+				
 		videoww = new VideoView(this);
 		RelativeLayout.LayoutParams videowwLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 		videowwLayoutParam.addRule(RelativeLayout.CENTER_IN_PARENT);
 		videoww.setLayoutParams(videowwLayoutParam);		
 		frelLayout.addView(videoww);	
 		
-		final ImageView loading = new ImageView(this);
+		loading = new ImageView(this);
 		loading.setImageDrawable(this.getResources().getDrawable((R.drawable.loading)));
 		RelativeLayout.LayoutParams loadingLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 		loadingLayoutParam.addRule(RelativeLayout.ALIGN_PARENT_TOP);
 		loading.setLayoutParams(loadingLayoutParam);
-		frelLayout.addView(loading);
+		frelLayout.addView(loading);	
 		
 	       mMediaController = new MediaController(this);
 	       videoww.setMediaController(mMediaController);
@@ -78,11 +92,10 @@ public class videocontroller extends Activity{
 	       mMediaController.setMediaPlayer(videoww);
 	       videoww.requestFocus();
 	       videoww.setDrawingCacheEnabled(true); 
-	       videoww.start();       
-	       
+	       videoww.start(); 
 	       RelativeLayout.LayoutParams frelLayoutLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 	       this.addContentView(frelLayout, frelLayoutLayoutParam);	
-       
+	       
        
 	    videoww.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
        {
@@ -90,7 +103,7 @@ public class videocontroller extends Activity{
 	            public void onCompletion(MediaPlayer mp){
 	       		videoww.setVisibility(View.INVISIBLE);
 	       		loading.setVisibility(View.VISIBLE);
-	       			//Toast.makeText(getBaseContext(), totle+">>>"+mediaurls.get(totle), Toast.LENGTH_SHORT).show();
+	       			
 						if(number==2)
 						{						
 						videoww.setVisibility(View.VISIBLE); 
@@ -109,12 +122,23 @@ public class videocontroller extends Activity{
        videoww.setOnPreparedListener(new OnPreparedListener() {
            @Override
            public void onPrepared(MediaPlayer mp) {
-        	   loading.setVisibility(View.INVISIBLE);  
-        	   videoww.start();  
-        	        
-           }
+         	   loading.setVisibility(View.INVISIBLE);  
+         	   videoww.start();
+            }
+           
        });
        
+       videoww.setOnErrorListener(new OnErrorListener(){
+		@Override
+		public boolean onError(MediaPlayer mp, int what, int extra) {
+			// TODO Auto-generated method stub
+			Toast.makeText(getBaseContext(), "连线中断，请重新点选课程。", Toast.LENGTH_SHORT).show();	
+			finish();
+			return false;
+		}
+    	   
+       });
+
       /* videoww.setOnKeyListener(new OnKeyListener(){
     	   @Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -124,6 +148,19 @@ public class videocontroller extends Activity{
        });*/
       
 	}
+	
+	 public boolean isNetworkConnected(Context context) {   
+		if (context != null) {   
+		 ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);   
+		 NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();   
+			 if (mNetworkInfo != null) {   
+				 return mNetworkInfo.isAvailable();   
+			 }   
+		}   
+		 return false;   
+	} 
+	
+	
 	public boolean dispatchKeyEvent (KeyEvent event) {
 		if (event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT
 		|| event.getKeyCode() == KeyEvent.KEYCODE_DPAD_UP 
